9773068d5c95448d19eec587c035494f50e3310a
umair abbas <umairabbas391@gmail.com>
2023-02-21 19:52:07 +0500

    changed the font stype of heading

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import FetchForm from "./components/FetchForm";
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-purple-400 to-blue-500 relative">
-      <p style={{
-        backgroundImage: 'linear-gradient(to right, #6B46C1, #4F46E5), linear-gradient(to right, white 50%, transparent 50%)',
-        WebkitBackgroundClip: 'text',
-        backgroundClip: 'text',
-        WebkitTextFillColor: 'transparent',
-      }} className="text-6xl font-bold absolute top-14 left-1/2 transform -translate-x-1/2 text-white opacity-40 z-10">Pass De</p>
+      <p
+        style={{
+          fontFamily: "Michroma, sans-serif",
+          backgroundImage:
+            "linear-gradient(to right,  #4F46E5, #6B46C1), linear-gradient(to right, white 0%, transparent 50%)",
+          WebkitBackgroundClip: "text",
+          backgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+        }}
+        className="text-7xl font-bold absolute top-14 left-1/2 transform -translate-x-1/2 text-white opacity-40 z-10"
+      >
+        Pass De
+      </p>
+
       <div className="row-span-3 row-start-2 row-end-4 w-screen flex justify-center items-center z-0">
         <Background>
           <FetchForm />
